943bcff25e62f569dc8a8331821284d8fe4f5420
RaylianuSerhii <s.i.goldenhot@gmail.com>
2021-08-08 19:32:36 +0300

    zadacha

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function Human(name, sex, lang, age) {
+    this.name = name;
+    this.sex = sex;
+    this.lang = lang;
+    this.age = age;
+}
+
+  
+Human.prototype.eldest = function(humans) {
+    let maxAge = 0;
+    humans.forEach(function(human) {
+        if (human.age > maxAge)
+          maxAge = human.age;
+        });
+    alert(maxAge);
+};
+  
+let human = new Human();
+let humans = [];
+humans.push(new Human("Ken", "male", "eng", 28));
+humans.push(new Human("Pol", "male", "eng", 22));
+humans.push(new Human("July", "female", "uk", 25));
+humans.push(new Human("Bill", "male", "rus", 36));
+    
+human.eldest(humans);
+  
